04364ec18bbdbed5893988c2d91e4cacf7906992
Syed Uns Haider Zaidi <unshaider28@gmail.com>
2024-05-10 02:47:46 +0500

    first commit

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answers = await inquirer.prompt([
-    {
-        type: "number",
-        name: "number1",
-        message: "Enter your first number:",
-    },
-    {
-        type: "number",
-        name: "number2",
-        message: "Enter your second number:",
-    },
     {
         type: "list",
         name: "operator",
@@ -24,22 +15,32 @@ const answers = await inquirer.prompt([
             "Modulus",
         ],
     },
+    {
+        type: "number",
+        name: "number1",
+        message: "Enter your first number:",
+    },
+    {
+        type: "number",
+        name: "number2",
+        message: "Enter your second number:",
+    },
 ]);
 if (answers.operator === "Addition") {
-    console.log(answers.number1 + answers.number2);
+    console.log((chalk.green.bold("Result: ")) + answers.number1 + answers.number2);
 }
 else if (answers.operator === "Subtraction") {
-    console.log(answers.number1 - answers.number2);
+    console.log((chalk.green.bold("Result: ")) + (answers.number1 - answers.number2));
 }
 else if (answers.operator === "Multiplication") {
-    console.log(answers.number1 * answers.number2);
+    console.log((chalk.green.bold("Result: ")) + answers.number1 * answers.number2);
 }
 else if (answers.operator === "Division") {
-    console.log(answers.number1 / answers.number2);
+    console.log((chalk.green.bold("Result: ")) + answers.number1 / answers.number2);
 }
 else if (answers.operator === "Exponentiation") {
-    console.log(answers.number1 ** answers.number2);
+    console.log((chalk.green.bold("Result: ")) + answers.number1 ** answers.number2);
 }
 else if (answers.operator === "Modulus") {
-    console.log(answers.number1 % answers.number2);
+    console.log((chalk.green.bold("Result: ")) + answers.number1 % answers.number2);
 }
--- main.ts
+++ main.ts
@@ -9,16 +9,6 @@ const answers: {
   operator: string;
 } = await inquirer.prompt([
   {
-    type: "number",
-    name: "number1",
-    message: "Enter your first number:",
-  },
-  {
-    type: "number",
-    name: "number2",
-    message: "Enter your second number:",
-  },
-  {
     type: "list",
     name: "operator",
     message: "Select the operation:",
@@ -31,18 +21,29 @@ const answers: {
       "Modulus",
     ],
   },
+  {
+    type: "number",
+    name: "number1",
+    message: "Enter your first number:",
+  },
+  {
+    type: "number",
+    name: "number2",
+    message: "Enter your second number:",
+  },
 ]);
 
+
 if (answers.operator === "Addition") {
-  console.log(answers.number1 + answers.number2);
+  console.log((chalk.green.bold("Result: "))+answers.number1 + answers.number2);
 } else if (answers.operator === "Subtraction") {
-  console.log(answers.number1 - answers.number2);
+  console.log((chalk.green.bold("Result: "))+(answers.number1 - answers.number2));
 } else if (answers.operator === "Multiplication") {
-  console.log(answers.number1 * answers.number2);
+  console.log((chalk.green.bold("Result: "))+answers.number1 * answers.number2);
 } else if (answers.operator === "Division") {
-  console.log(answers.number1 / answers.number2);
+  console.log((chalk.green.bold("Result: "))+answers.number1 / answers.number2);
 } else if (answers.operator === "Exponentiation") {
-  console.log(answers.number1 ** answers.number2);
+  console.log((chalk.green.bold("Result: "))+answers.number1 ** answers.number2);
 } else if (answers.operator === "Modulus") {
-  console.log(answers.number1 % answers.number2);
-} 
+  console.log((chalk.green.bold("Result: "))+answers.number1 % answers.number2);
+}
